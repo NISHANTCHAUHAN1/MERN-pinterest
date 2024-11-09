cf5a2d33d4e40aac3e465d9f1ecad950e7cc99da
NISHANTCHAUHAN1 <onenish14@gmail.com>
2024-11-09 13:40:04 -0500

    minor updates

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -20,18 +20,6 @@ const Home = () => {
         {loading ? (
           <Loading />
         ) : (
-          // <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-          //   <div className="px-4 py-6 sm:px-0">
-          //     <div className="flex flex-wrap m-4">
-          //       {pins && pins.length > 0 ? (
-          //         pins.map((e, i) => <PinCard key={i} pin={e} />)
-          //       ) : (
-          //         <p>No Pins Yet</p>
-          //       )}
-          //     </div>
-          //   </div>
-          // </div>
-
           <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
             <div className="px-4 py-6 sm:px-0">
               <Masonry
